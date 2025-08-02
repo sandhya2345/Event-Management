290af404948276f7aa01e0a704048b6b9b1d916b
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-08-02 20:09:11 +0545

    changes done in calendar

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -18,10 +18,10 @@ export const CalendarView = () => {
   return (
     <div className="pt-24 px-4 min-h-screen bg-primary">
       {/* <h1 className="text-3xl font-bold mb-2 text-center text-white">Event Calendar</h1> */}
-       <p className="text-center text-gray-100 mb-6 max-w-2xl mx-auto">
+       {/* <p className="text-center text-gray-100 mb-6 max-w-2xl mx-auto">
     Welcome to the event calendar! Here you can find upcoming events and important details. Click any event for more information.
-  </p>
-      <div className="bg-white max-w-5xl mx-auto p-4 text-primary">
+  </p> */}
+      <div className="bg-white max-w-5xl mx-auto p-6 rounded  text-primary">
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
