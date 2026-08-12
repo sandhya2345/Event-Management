4939062cbee409cd7b65599976206a7927f69531
Sandhya Timalsena <134833781+sandhya2345@users.noreply.github.com>
2026-08-12 13:07:56 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
 
                     {/* Download Button 1 */}
                     <a
-                        href="/Day_4_Working_with_LLM (1).ipynb"
+                        href="/day5_vector_Store.ipynb"
                         download
                         className={`flex items-center gap-2 px-5 py-2 rounded-lg font-medium transition duration-300 ${
                             isScrolled
@@ -241,7 +241,7 @@ const Navbar = () => {
                     {/* Mobile Download Button 1 */}
                     <li>
                         <a
-                            href="/Day_4_Working_with_LLM (1).ipynb"
+                            href="/day5_vector_Store.ipynb"
                             download
                             onClick={() =>
                                 setMenuOpen(false)
