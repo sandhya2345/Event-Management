830d239c7e8bbf1f97f4e18494169192fbe76ef4
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2026-08-19 21:11:10 +0545

    done something i guess

--- src/pages/RecentActivity.jsx
+++ src/pages/RecentActivity.jsx
@@ -52,7 +52,7 @@ const RecentActivity = () => {
   };
 
   return (
-    <div className="min-h-full px-4 sm:px-6 md:px-12 lg:px-20 py-10 bg-white">
+    <div className="hidden min-h-full px-4 sm:px-6 md:px-12 lg:px-20 py-10 bg-white">
       <h2 className="text-3xl md:text-4xl text-primary font-bold mb-10 text-center">
         Recent Activity
       </h2>
